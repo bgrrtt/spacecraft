6a57c46549cf1e44c1625d9abb247d03ca0d8e96
Branden Garrett <branden.garrett@gmail.com>
2017-07-29 13:08:37 -0400

    imports operations/

---
+++ operations/vagrant/install.rb
@@ -0,0 +1,43 @@
+################################################################################
+required_plugins = %w( vagrant-hostsupdater vagrant-vbguest vagrant-bindfs )
+required_plugins.each do |plugin|
+    exec "vagrant plugin install #{plugin};vagrant #{ARGV.join(" ")}" unless Vagrant.has_plugin? plugin || ARGV[0] == 'plugin'
+end
+################################################################################
+#
+################################################################################
+require 'yaml'
+
+# Recursively walk an tree and run provided block on each value found.
+def walk(obj, &fn)
+  if obj.is_a?(Array)
+    obj.map { |value| walk(value, &fn) }
+  elsif obj.is_a?(Hash)
+    obj.each_pair { |key, value| obj[key] = walk(value, &fn) }
+  else
+    obj = yield(obj)
+  end
+end
+
+# Resolve jinja variables in hash.
+def resolve_jinja_variables(vconfig)
+  walk(vconfig) do |value|
+    while value.is_a?(String) && value.match(/{{ .* }}/)
+      value = value.gsub(/{{ (.*?) }}/) { vconfig[Regexp.last_match(1)] }
+    end
+    value
+  end
+end
+
+# Return the combined configuration content all files provided.
+def load_config(files)
+  vconfig = {}
+  files.each do |config_file|
+    if File.exist?(config_file)
+      optional_config = YAML.load_file(config_file)
+      vconfig.merge!(optional_config) if optional_config
+    end
+  end
+  resolve_jinja_variables(vconfig)
+end
+################################################################################
